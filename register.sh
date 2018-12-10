e4a7e6f1b76d14b1b66fa015eaf03f31420f91dc
Radek Strejc <radek.strejc@oracle.com>
2018-12-10 11:16:38 +0100

    getopts fixes

--- register.sh
+++ register.sh
@@ -9,50 +9,58 @@ function fail() { err "${@}"; exit 254; }
 SCRIPT=$(readlink -f "${0}")
 SCRIPT_NAME=$(basename "${SCRIPT}")
 
+DEFAULT_TAGS="docker"
+DEFAULT_IMAGE="debian:sid-slim"
+
 show_help() {
 inf "Usage: ${SCRIPT_NAME} [OPTIONS] 
 
 Register new gitlab runner.
 
 Options:
-    -T <token>  gitlab token (mandatory)
+    -R <runner> runner name (mandatory)
     -U <url>    gitlab url (mandatory)
-    -r <name>   runner name
-    -i <image>  default image
-    -t <tags>   runner tags
+    -T <token>  gitlab token (mandatory)
+    -i <image>  default image (default ${DEFAULT_IMAGE})
+    -t <tags>   runner tags (default ${DEFAULT_TAGS})
     -h,-?       display this help and exit
 
 Environments:
-    TOKEN, URL, RUNNER, CONFIG_DIRECTORY, IMAGE, TAGS"
+    TOKEN, URL, RUNNER, IMAGE, TAGS
+
+Examples:
+    ${SCRIPT_NAME} -R my-runner -U our.gitlab.com -T token-value
+    RUNNER=my-runner URL=our.gitlab.com TOKEN=token-value ${SCRIPT_NAME}
+"
         exit 1
 }
 
 OPTIND=1
-while getopts "r:d:t:U:i:T:c:h" OPT; do
+while getopts "R:T:U:i:t:c:h" OPT; do
     case "${OPT}" in
-        "r")    RUNNER="${OPTARG}";;
+        "R")    RUNNER="${OPTARG}";;
         "T")    TOKEN="${OPTARG}";;
         "U")    URL="${OPTARG}";;
         "i")    IMAGE="${OPTARG}";;
         "t")    TAGS="${OPTARG}";;
-        "c")    CONTAINER_NAME="${OPTARG}";;
         "h"|"?")show_help;;
         "*")    echo "Unknown option: -${OPTARG}. Run with -h to display help";;
     esac
 done
 shift "$((OPTIND-1))"   
 
+if [[ ! ${RUNNER+x} ]]; then fail "Missing argument: runner"; fi
 if [[ ! ${TOKEN+x} ]]; then fail "Missing argument: token"; fi
 if [[ ! ${URL+x} ]]; then fail "Missing argument: url"; fi
-TAGS="${TAGS:-"docker"}"
+TAGS="${TAGS:-"${DEFAULT_TAGS}"}"
+IMAGE="${IMAGE:-"${DEFAULT_IMAGE}"}"
 
 REGEX="^[a-z0-9]([a-z0-9-]*[a-z]+)*$"
 if [[ ! "${RUNNER}" =~ ${REGEX} ]]; then
-        fail "Runner name ${RUNNER} does not match ${REGEX}"
+    fail "Runner name ${RUNNER} does not match ${REGEX}"
 fi
 
 inf "Registering ${RUNNER}"
-
 gitlab-ci-multi-runner register \
     --non-interactive \
     --executor "docker" \
